4ffadd0a809707a1bf58c58dede223762d8d2763
1alberto994 <alby94ct@gmail.com>
2023-03-07 09:50:18 +0100

    esercizio completato

--- Classes/cats.php
+++ Classes/cats.php
@@ -1,12 +1,14 @@
 <?php
-require_once __DIR__.'product.php';
+require_once __DIR__.'/product.php';
 class cats extends products{
     public $razza;
+    public $URL;
 
-    public function __construct($_razza,$cibo,$giochi,$cucce)
+    public function __construct($_razza,$URL,$cibo,$giochi,$cucce)
     {
         parent:: __construct($cibo,$giochi,$cucce);
          $this->razza=$_razza;
+         $this->URL=$URL;
     }
 }
 
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__ .'/Classes/cats.php';
 require_once __DIR__ .'/Classes/product.php';
 
@@ -12,8 +15,44 @@ var_dump($prodotti);
 
 
 
-$cat=new cats('bengala','croccantini',['Cannetta con topino sopra',
+$cat=new cats('bengala','https://www.gattidelbengala.it/wp-content/uploads/2020/04/Schermata-2020-04-15-alle-11.54.10.png','croccantini',['Cannetta con topino sopra',
 'Tiragraffi per gatto'],
  'casetta');
 
 var_dump($cat);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <div>
+            <h1>Gatto Bengala</h1>
+            <img src="https://www.gattidelbengala.it/wp-content/uploads/2020/04/Schermata-2020-04-15-alle-11.54.10.png" alt="">
+            <ul>
+                <li>
+                    <?php echo $razza;?>
+                </li>
+                <li>
+                <?php echo $cibo?>
+                </li>
+                <li>
+                <?php echo $giochi?>
+                </li>
+                <li>
+                <?php echo $cucce?>
+                </li>
+            </ul>
+        </div>
+    </main>
+</body>
+</html>
